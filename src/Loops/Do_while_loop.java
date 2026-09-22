0ff52a9b998821dc2070c0479c486c7a996ea40d
Manoj <sahanimanoj2324@gmail.com>
2026-09-22 16:15:47 +0530

    add Loops

---
+++ src/Loops/Do_while_loop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Do_while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.print(i + " ");
+            i = i + 1;
+        }while (i < 11);
+    }
+}
---
+++ src/Loops/For_loop.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class For_loop {
+    public static void main(String[] args) {
+        // i++ => i = i + 1
+        for (int i = 0; i < 11; i++){
+            System.out.print(i + " ");
+        }
+    }
+}

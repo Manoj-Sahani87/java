464461b47e1b58eb40c9d0675bedef1ffd819d43
Manoj <sahanimanoj2324@gmail.com>
2026-09-23 20:25:09 +0530

    add Patterns

---
+++ src/Patterns/Pattern_1.java
@@ -0,0 +1,17 @@
+package Patterns;
+// Solid Rectangle
+public class Pattern_1 {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+
+        // Outer Loop
+        for (int i = 1; i <= n; i++){
+            // Inner loop
+            for (int j = 1; j <= m; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
